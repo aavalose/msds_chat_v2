fd5263e178f1cb1991804446b6fcabd971444265
Arturo Avalos <aavalosespinoza@gpu3.datascience.usfca.edu>
2025-03-24 11:07:12 -0700

    debug 3

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="msds_chat_demo",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "streamlit>=1.31.0",
+        "torch==2.2.0",
+        "numpy>=1.24.0",
+        "pillow==9.1.0",
+        "pandas>=2.0.0",
+        "transformers",
+        "sentence-transformers",
+        "accelerate>=0.26.0",
+        "google-generativeai>=0.3.0",
+        "chromadb>=0.4.0",
+        "pysqlite3-binary>=0.5.0",
+        "pymongo[srv]",
+        "dnspython",
+    ],
+) 
